726ba7a0adec37c7942352044f772f1b273cc73d
lizcramerfox <ecfox2008@gmail.com>
2020-03-12 12:20:39 -0400

    JS can add an X to any box that is valid (empty)
    Code added in events.js

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,7 +2,7 @@ const api = require('./api')
 const ui = require('./ui')
 
 // ADD LATER (double-check filepath - this is copied from jquery-ajax-crud training)
-  // const getFormFields = require('./../../../lib/get-form-fields')
+// const getFormFields = require('./../../../lib/get-form-fields')
 const boardItems = [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined]
 
 // Make a function that selects a box when the user clicks it
@@ -12,6 +12,7 @@ const makeMove = function (event) {
   console.log(selectedBox)
   if (selectedBox !== undefined) {
     console.log('box is available')
+    $(selectedBox).text('x')
   }
 }
 
