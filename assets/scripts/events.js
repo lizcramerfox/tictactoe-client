cc9a370c488e993c71cc538ccaf6464c2e94fe39
lizcramerfox <ecfox2008@gmail.com>
2020-03-13 13:15:17 -0400

    Gameboard functioning; checkForWinner test button
    - Gameboard functions well now
    - checkForWinner button added to html so you can play a game and test
      for a winner. HOWEVER:
        - doesn't differentiate between existing "null" placeholders (so
          it's always a win upon load & null-null-null matches)

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,13 +5,17 @@
 // ADD LATER (double-check filepath - this is copied from jquery-ajax-crud training)
 // const getFormFields = require('./../../../lib/get-form-fields')
 
+const gameboard = [null, null, null, null, null, null, null, null, null]
+
 // USER SELECTS & MARKS UP BOARD w/ MARKERS
 let currentPlayer = 'X'
 // Make a function that selects a box when the user clicks it
 const selectBox = function (event) {
-  event.preventDefault()
-  console.log(event.target.id)
   const selectedBox = event.target
+  const boxId = event.target.id
+  if (gameboard[boxId] === null) {
+    gameboard[boxId] = currentPlayer
+  }
   const selectedBoxText = $(selectedBox).text() // '' , 'X', 'O'
   // if space is empty then add play
   if (selectedBoxText === '') {
@@ -23,16 +27,31 @@ const selectBox = function (event) {
     } else {
       currentPlayer = 'X'
     }
-  // else invalid move
+    // else invalid move
   } else {
     console.log('box has already been taken')
   }
 }
-
-const gameboard = [null, null, null, null, null, null, null, null, null]
-
-
+// CHECK FOR WINNER:
+// Pass the array through a function to check if:
+const checkForWinner = function () {
+  // if ANY of the following are true:
+  if (
+    ((gameboard[0] === gameboard[1] && gameboard[1] === gameboard[2])) || // 0=1=2 OR
+    ((gameboard[3] === gameboard[4] && gameboard[4] === gameboard[5])) || // 3=4=5
+    ((gameboard[6] === gameboard[7] && gameboard[7] === gameboard[8])) || // 6=7=8
+    ((gameboard[0] === gameboard[3] && gameboard[3] === gameboard[6])) || // 0=3=6
+    ((gameboard[1] === gameboard[4] && gameboard[4] === gameboard[7])) || // 1=4=7
+    ((gameboard[2] === gameboard[5] && gameboard[5] === gameboard[8])) || // 2=5=8
+    ((gameboard[0] === gameboard[4] && gameboard[4] === gameboard[8])) || // 0=4=8
+    ((gameboard[6] === gameboard[4] && gameboard[4] === gameboard[2]))) { // 6=4=2
+    console.log('winner!')
+  } else {
+    console.log('game in progress - make a move')
+  }
+}
 
 module.exports = {
-  selectBox
+  selectBox,
+  checkForWinner
 }
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,8 +1,3 @@
-// const onMakeMove = function (boxId) {
-//
-//   $('.box').addClass('s')
-// }
-
 // export ui functions so they can be used in events.js
 // if your key has the same name as its value, you can just write the name once
 module.exports = {
